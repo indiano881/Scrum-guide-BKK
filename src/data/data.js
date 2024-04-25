8b1468eb1b834b1f589dff0fcda165bb7d47f1e8
Davide Baldi <d.baldi88@gmail.com>
2024-04-25 14:02:27 +0200

    BKK-36 data.js created, constructor created, 1 object paragraph 1 crieted

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+function Article(title, image, content1, content2, content3 ) {
+    this.title=title,
+    this.image=image,
+    this.content=content
+}
+//PAR 1
+const Agile= new Article (
+    "What is Agile Project Metodology?",
+    "INSERT_IMAGE",
+    "Agile methodologies are a set of methods and approaches in software development that prioritize flexibility, collaboration, and continuous improvement.",
+    "They aim to adapt to rapidly changing requirements, incorporate customer feedback effectively, and enhance the software development process.",
+    "The goal of Agile is to respond better to changing requirements, evaluate customer feedback more effectively, and make the software development process more flexible and adaptable compared to traditional methods."
+)
