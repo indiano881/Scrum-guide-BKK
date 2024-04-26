35e6a0db05d0ba65822385391d49adb357b2551c
Carl Åberg <caaberg99@gmail.com>
2024-04-26 14:49:46 +0200

    Added Footer + styling + react-fonts npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Footer from "../src/components/Footer"
 
 function App() {
   
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
      <h1>hello world</h1>
+     <Footer/>
     </>
   )
 }
