27d149a2b3d50cdc51661aa68c319eda80d0eef0
bedirgcmz <bdrgcmz@gmail.com>
2024-04-26 19:14:10 +0200

    Created Header component and it done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { getImageURL } from "../utils/functions.js";
-import Scrum from "./components/Scrum"
+import Scrum from "./components/Scrum";
 import paragraphArray from "./data/data.js";
 import Office from "./components/Office/index.jsx";
 import ContactUs from "./components/ContactUs/index.jsx";
-import Footer from "../src/components/Footer"
+import Footer from "../src/components/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-     <h1>hello world</h1>
-      <h1>hello world</h1>
-      {/* testing img functions*/}
-      <img src={getImageURL("logo.png")} alt="" />
-      {paragraphArray.map((paragraph, index)=> <Scrum  key={index} {...paragraph}/>)}
+      <Header />
+      {paragraphArray.map((paragraph, index) => (
+        <Scrum key={index} {...paragraph} />
+      ))}
 
       <ContactUs />
       <Office />
-      <Footer/>
+      <Footer />
     </>
   );
 }
