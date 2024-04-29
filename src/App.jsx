eb8bf0fe9b1def90de03d647d637c896d138d724
bedirgcmz <bdrgcmz@gmail.com>
2024-04-29 22:29:49 +0200

    added id to link in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { getImageURL } from "../utils/functions.js";
+<<<<<<< HEAD
+=======
+import Footer from "../src/components/Footer";
+import ContactUs from "./components/ContactUs/index.jsx";
+>>>>>>> f9ce240 (added id to link in header)
 import Scrum from "./components/Scrum";
 import paragraphArray from "./data/data.js";
 import Office from "./components/Office/index.jsx";
@@ -8,16 +13,22 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
+    <div id="home">
       <Header />
       {paragraphArray.map((paragraph, index) => (
         <Scrum key={index} {...paragraph} />
       ))}
+<<<<<<< HEAD
 
       <ContactUs />
       <Office />
       <Footer />
     </>
+=======
+      <ContactUs />
+      <Footer />
+    </div>
+>>>>>>> f9ce240 (added id to link in header)
   );
 }
 
