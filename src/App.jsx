699686c36347fc27980c0895a80e8962a98b3816
Davide Baldi <d.baldi88@gmail.com>
2024-04-29 13:57:30 +0200

    BKK-31 component created with basic stylyng

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { getImageURL } from "../utils/functions.js";
-
+import ContactUs from "./components/ContactUs/index.jsx";
 function App() {
   return (
     <>
       <h1>hello world</h1>
       {/* testing img functions*/}
       <img src={getImageURL("logo.png")} alt="" />
+      <ContactUs />
     </>
   );
 }
---
+++ src/components/ContactUs/index.jsx
@@ -0,0 +1,19 @@
+import styles from './ContactUs.module.css'
+
+const ContactUs = () => {
+    return (
+        <>
+          <div className={styles.container}>
+
+            <h3 className={styles.contactTitle}>Contact Us</h3>
+                <div>
+                    Our Team of expert
+                
+                </div>
+            
+          </div>
+        </>
+    )
+};
+
+export default ContactUs
