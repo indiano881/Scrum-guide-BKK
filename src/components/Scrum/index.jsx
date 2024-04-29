df71937426fa0e39d9cf0a7a183ccb75983edfda
Shada Baz <shada.bazger@edu.cmeducations.se>
2024-04-29 12:14:12 +0200

    comp done

--- src/components/Scrum/index.jsx
+++ src/components/Scrum/index.jsx
@@ -1,5 +1,16 @@
-const Scrum = () => {
+
+
+const Scrum = ({title, content1, content2, content3}) => {
     return (
-        <h3></h3>
+        
+        <div>
+            <h3>{title}</h3>
+            <p>{content1}</p>
+            <p>{content2}</p>
+            <p>{content3}</p>
+    
+        </div>
     )
 }
+
+export default Scrum ;
