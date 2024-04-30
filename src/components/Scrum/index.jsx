cabdd44d3e0f96cd9d392aae521b896d729d5104
Davide Baldi <d.baldi88@gmail.com>
2024-04-30 18:59:41 +0200

    fixed small issue with text

--- src/components/Scrum/index.jsx
+++ src/components/Scrum/index.jsx
@@ -1,10 +1,13 @@
-const Scrum = ({ title, content1, content2, content3, linkText }) => {
+const Scrum = ({ title, content1, content2, content3, content4, content5, content6, linkText }) => {
   return (
     <div id={linkText}>
       <h3>{title}</h3>
       <p>{content1}</p>
       <p>{content2}</p>
       <p>{content3}</p>
+      <p>{content4}</p>
+      <p>{content5}</p>
+      <p>{content6}</p>
     </div>
   );
 };
