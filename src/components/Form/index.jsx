b33771f1e9bdde5fd719f4ac9ffade3c34e498ad
Davide Baldi <d.baldi88@gmail.com>
2024-05-01 15:28:22 +0200

    BKK-33 message field and button

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,11 +2,14 @@ import styles from './Form.module.css'
 
 const Form = () => {
     return (
-        <form>
-          <label for="name">Name</label>
+        <form action="mailto: scrum-bkk@gmail.com" method="POST" enctype="text/plain">
+          <label for="name" minlength="3" required>Name</label>
           <input id='name' type="text" placeholder='Insert your name...'/>
-          <label for="name">Phone</label>
+          <label for="name" minlength="6">Phone</label>
           <input id='name' type="text" placeholder='Insert your phone...'/>
+          <label for="message"></label>
+          <textarea name="message" id="message" cols="30" rows="10" placeholder="Please write your message here..." required></textarea>
+          <button>Send message</button>
         </form>
     )
 };
