d13309cc52a097da9bfe7328dca5dec60e7b62c0
bedirgcmz <bdrgcmz@gmail.com>
2024-05-02 13:07:37 +0200

    Fixed conflict again

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import ContactUs from "./components/ContactUs/index.jsx";
 import Scrum from "./components/Scrum";
 import paragraphArray from "./data/data.js";
 import Office from "./components/Office/index.jsx";
-import ContactUs from "./components/ContactUs/index.jsx";
-import Footer from "../src/components/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
