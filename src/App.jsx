50c906052efea2ed765cc9c103f702ba82a968cd
bedirgcmz <bdrgcmz@gmail.com>
2024-05-02 13:07:37 +0200

    Fixed conflict again

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { getImageURL } from "../utils/functions.js";
 import Scrum from "./components/Scrum";
 import paragraphArray from "./data/data.js";
 import Office from "./components/Office/index.jsx";
-import ContactUs from "./components/ContactUs/index.jsx";
-import Footer from "../src/components/Footer";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/index.jsx";
 import BackTop from "./components/BackTop";
