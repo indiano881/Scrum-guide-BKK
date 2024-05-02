9fbc7a7588bc5f8b4e163e8e1fec2abf0814ed69
bedirgcmz <bdrgcmz@gmail.com>
2024-05-02 13:17:21 +0200

    Added id for about us

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -4,8 +4,8 @@ import staffArray from "../../data/staff";
 const ContactUs = () => {
   return (
     <>
-      <div id="contact" className={styles.container}>
-        <h3 className={styles.contactTitle}>Contact Us</h3>
+      <div id="about-us" className={styles.container}>
+        <h3 className={styles.contactTitle}>About Us</h3>
         <div>
           <h3 className={styles.contactTitle}>Our Team of Expert</h3>
           <div className={styles.gridContainer}>
