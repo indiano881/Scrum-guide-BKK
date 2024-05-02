f0d03100122c9ddf5682386601c61998e24013d8
Davide Baldi <d.baldi88@gmail.com>
2024-05-02 19:50:35 +0200

    fixed navigation

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,7 +4,7 @@ import {getImageURL} from "../../utils/functions"
 const Form = () => {
     return (
       <>
-      <h2 className={styles.title}>Contact Us</h2>
+      <h2 className={styles.title} id="contact">Contact Us</h2>
       <div className={styles.container}>
         
         <form className={styles.formContainer} action="mailto: scrum-bkk@gmail.com" method="POST" enctype="text/plain">
