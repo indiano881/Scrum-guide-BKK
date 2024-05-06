accd7fb7509c1cb3f18d2a0390933c8dcbfc7730
bedirgcmz <bdrgcmz@gmail.com>
2024-05-06 10:42:37 +0200

    fixedd conflictt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./styles/global.css";
-import { getImageURL } from "../utils/functions.js";
 import Scrum from "./components/Scrum";
 import paragraphArray from "./data/data.js";
 import Office from "./components/Office/index.jsx";
+import ContactUs from "./components/ContactUs/index.jsx";
+import Footer from "./components/Footer/index.jsx";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/index.jsx";
 import BackTop from "./components/BackTop";
